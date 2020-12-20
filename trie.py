cc2f6b50a40f16cdf7704fbace1f80db1b0d6920
sudbasnet <sudbasnet10@gmail.com>
2020-12-20 13:54:17 -0600

    more optimized trie datastructre

--- trie.py
+++ trie.py
@@ -1,5 +1,5 @@
 import collections
-
+'''
 class TrieNode:
     def __init__(self):
         self.children = collections.defaultdict(TrieNode)
@@ -59,3 +59,57 @@ obj.insert('apple')
 print(obj.search('appe'))
 print(obj.allWordsStartingWith('c'))
 # param_3 = obj.startsWith(prefix)
+'''
+## second trie 
+
+class Trie:
+    def __init__(self):
+        self.node = {}
+    
+    def insert(self, word):
+        current = self.node
+        for letter in word:
+            if letter not in current: current[letter] = {}
+            current = current[letter]
+        current["word"] = word
+
+    def search(self, word):
+        current = self.node
+        for letter in word:
+            if letter in current: current = current[letter]
+            else: return False
+        if word in current: return True
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        current = self.node
+        for pre in prefix:
+            if pre in current: current = current[pre]
+            else: return False
+        return True
+
+    def wordsStartingWith(self, prefix: str) -> [str]:
+        matchedWords = []
+        current = self.node
+        for pre in prefix:
+            if pre in current: current = current[pre]
+            else: return []
+        # current trie at this point has all the words
+        stack = [current]
+        while stack:
+            current = stack.pop()
+            if 'word' in current: 
+                matchedWords.append(current['word'])
+            for key, val in current.items():
+                # add all except for the one with the 'word' key
+                if key != 'word': stack.append(val)
+        return matchedWords
+
+obj = Trie()
+obj.insert('car')
+obj.insert('card')
+obj.insert('con')
+obj.insert('apple')
+print(obj.search('appe'))
+print(obj.wordsStartingWith('c'))
+# print(obj.node)
