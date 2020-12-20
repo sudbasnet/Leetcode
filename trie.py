5f37920a9dba5f9a697226bc5570b75db2cc3756
sudbasnet <sudbasnet10@gmail.com>
2020-12-20 12:44:13 -0600

    TRIE data structure

--- trie.py
+++ trie.py
@@ -1,15 +1,61 @@
-def buildTrie(self, words):
-    self.trie = {}
-    for i in range(len(words)):
-        # for every word, reset to root
-        node = self.trie
+import collections
+
+class TrieNode:
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.word = None
         
-        for char in words[i]:
-            if char in node:
-                node = node[char]
-            else:
-                newNode = {}
-                newNode['#'] = []
-                node[char] = newNode
-                node = newNode
-            node["#"].append(i)
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        current = self.root
+        for letter in word:
+            current = current.children[letter]
+        current.word = word
+
+    def search(self, word: str) -> bool:
+        current = self.root
+        for letter in word:
+            current = current.children.get(letter)
+            if not current:
+                return False
+        if current.word:
+            return True
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        current = self.root
+        for pre in prefix:
+            current = current.children.get(pre)
+            if not current:
+                return False
+        return True
+
+    def allWordsStartingWith(self, prefix: str) -> [str]:
+        matchedWords = []
+        current = self.root
+        for pre in prefix:
+            current = current.children.get(pre)
+            if not current:
+                return []
+        # current trie at this point has all the words
+        stack = [current]
+        while stack:
+            current = stack.pop()
+            if current.word:
+                matchedWords.append(val.word)
+            for val in current.children.values():
+                stack.append(val)
+        return matchedWords
+
+# Your Trie object will be instantiated and called as such:
+obj = Trie()
+obj.insert('car')
+obj.insert('card')
+obj.insert('con')
+obj.insert('apple')
+print(obj.search('appe'))
+print(obj.allWordsStartingWith('c'))
+# param_3 = obj.startsWith(prefix)
